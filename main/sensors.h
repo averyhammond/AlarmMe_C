31d943398bd58ccfc6b73816ed5708c4cf49c4f5
averyhammond <hammon34@pudue.edu>
2022-04-14 13:41:02 -0400

    Reading from 3 sensors

--- main/sensors.h
+++ main/sensors.h
@@ -19,11 +19,13 @@
 void init(void);
 void ADC_init(void);
 void GPIO_init(void);
+void I2C_init(void);
 void display_sensor_data(void);
 void run(void);
 void get_sensor_data(void);
 uint32_t get_temp_data(void);
 uint32_t get_pres_data(void);
+uint32_t get_co2_data(void);
 void process_data(void);
 void clear_flags(void);
 void set_alarms(void);
@@ -61,6 +63,7 @@ void init(void)
 {
     ADC_init();
     GPIO_init();
+    I2C_init();
 
     return;
 }
@@ -98,10 +101,38 @@ void GPIO_init()
 }
 
 
+// Function to initialize all the necessary I2C connections
+void I2C_init()
+{
+    i2c_config_t config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = 23,
+        .scl_io_num = 22,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 400000,
+    };
+
+    i2c_param_config(I2C_NUM_0, &config);
+    i2c_driver_install(I2C_NUM_0, config.mode, 0, 0, 0);
+
+    uint8_t init[2] = {0x20, 0x03};
+    uint8_t meas[2] = {0x20, 0x08};
+    
+    i2c_master_write_to_device(I2C_NUM_0, 0x58, init, 2, 1000 / portTICK_RATE_MS);
+    vTaskDelay(10 / portTICK_RATE_MS);
+
+    i2c_master_write_to_device(I2C_NUM_0, 0x58, meas, 2, 1000 / portTICK_RATE_MS);
+    vTaskDelay(100 / portTICK_RATE_MS);
+
+    return;
+}
+
+
 // Debugging function to print current sensor data to terminal
 void display_sensor_data()
 {
-    printf("Temp: %d\nPres: %d\n", Sensors.curr_temp, Sensors.curr_pres);
+    printf("Temp: %d\nPres: %d\nCO2: %d\n", Sensors.curr_temp, Sensors.curr_pres, Sensors.curr_co2);
 
     return;
 }
@@ -112,6 +143,7 @@ void get_sensor_data(void)
 {
     Sensors.curr_temp = get_temp_data();
     Sensors.curr_pres = get_pres_data();
+    Sensors.curr_co2 = get_co2_data();
 
     Sensors = Sensors;  // Errors otherwise
 
@@ -160,8 +192,6 @@ void set_alarms()
         Sensors.alarm_temp = 1;
     }
 
-    // TODO: Check CO2 readings
-
     return;
 }
 
@@ -193,4 +223,24 @@ uint32_t get_pres_data(void)
     adc1_ch3_pres += adc1_get_raw((adc1_channel_t) ADC_CHANNEL_3);
         
     return adc1_ch3_pres;
+}
+
+
+// Function to read the CO2 in ppm via I2C
+uint32_t get_co2_data()
+{
+    uint32_t co2_data = 0;
+    uint8_t meas[2] = {0x20, 0x08};
+    uint8_t res[6];
+
+    i2c_master_write_to_device(I2C_NUM_0, 0x58, meas, 2, 1000 / portTICK_RATE_MS);
+    vTaskDelay(100 / portTICK_RATE_MS);
+
+    i2c_master_read_from_device(I2C_NUM_0, 0x58, res, 6, 1000 / portTICK_RATE_MS);
+    vTaskDelay(6 / portTICK_RATE_MS);
+
+    uint8_t temp_res[2] = {res[0], res[1]};
+    co2_data = temp_res[0] << 8 | temp_res[1];
+
+    return co2_data;
 }
